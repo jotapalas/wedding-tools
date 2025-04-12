1b7d658be571d6d180d7fa1e83a95a8b9fcb36ec
Juan Palacios <juan@jotapalas.dev>
2025-04-13 00:27:50 +0200

    add fonts and fine tune styling

--- frontend/src/sections/FAQ/FAQ.jsx
+++ frontend/src/sections/FAQ/FAQ.jsx
@@ -4,34 +4,34 @@ import SectionTitle from '../../components/sectionTitle/SectionTitle';
 import FAQItem from '../../components/faqItem/FAQItem';
 
 function FAQ() {
-    const questions = [
-        {
-            question: "¿Puedo comprar un producto sin registrarme?",
-            answer: "Sí, puedes comprar sin registrarte. Sin embargo, te recomendamos que te registres para facilitar futuras compras y acceder a promociones exclusivas."
-        },
-        {
-            question: "¿Cuáles son los métodos de pago aceptados?",
-            answer: "Aceptamos tarjetas de crédito, débito, PayPal y transferencias bancarias."
-        },
-        {
-            question: "¿Cómo puedo rastrear mi pedido?",
-            answer: "Una vez que tu pedido haya sido enviado, recibirás un correo electrónico con un enlace de seguimiento."
-        },
-        {
-            question: "¿Puedo devolver un producto?",
-            answer: "Sí, puedes devolver un producto dentro de los 30 días posteriores a la compra. Asegúrate de que esté en su estado original."
-        },
-    ];
-    return (
-        <div className="faq">
-        <SectionTitle title="FAQ" />
-        <div className="faq-content">
-            {questions.map((item, index) => (
-                <FAQItem key={index} question={item.question} answer={item.answer} />
-            ))}
-        </div>
-        </div>
-    );
+  const questions = [
+    {
+      question: "¿Puedo comprar un producto sin registrarme?",
+      answer: "Sí, puedes comprar sin registrarte. Sin embargo, te recomendamos que te registres para facilitar futuras compras y acceder a promociones exclusivas."
+    },
+    {
+      question: "¿Cuáles son los métodos de pago aceptados?",
+      answer: "Aceptamos tarjetas de crédito, débito, PayPal y transferencias bancarias."
+    },
+    {
+      question: "¿Cómo puedo rastrear mi pedido?",
+      answer: "Una vez que tu pedido haya sido enviado, recibirás un correo electrónico con un enlace de seguimiento."
+    },
+    {
+      question: "¿Puedo devolver un producto?",
+      answer: "Sí, puedes devolver un producto dentro de los 30 días posteriores a la compra. Asegúrate de que esté en su estado original."
+    },
+  ];
+  return (
+    <section className="faq">
+      <SectionTitle title="FAQ" />
+      <div className="faq-content">
+        {questions.map((item, index) => (
+          <FAQItem key={index} question={item.question} answer={item.answer} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default FAQ;
--- frontend/src/sections/Hero/Hero.jsx
+++ frontend/src/sections/Hero/Hero.jsx
@@ -1,15 +1,38 @@
 import './Hero.css';
-import banner from '../../img/hero-banner.jpg';
 
-function Hero() {
-    return (
-      <section className="hero">
-        <div className="hero-content">
-          <img src={banner} alt="Hero" className="hero-image" />
-        </div>
-      </section>
-    );
-  }
+import { useState, useEffect } from 'react';
+
+import mobileBanner from '../../img/mobile-banner.svg';
+import desktopBanner from '../../img/desktop-banner.svg';
+
+function Hero() { 
+  const [width, setWidth] = useState(window.innerWidth);
+  const [isMobile, setIsMobile] = useState(width <= 768);
   
-  export default Hero;
+  useEffect(() => {
+    console.log('useEffect called');
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const banner = isMobile ? mobileBanner : desktopBanner;
+
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <img src={banner} alt="Hero" className="hero-image" />
+      </div>
+    </section>
+  );
+}
+  
+export default Hero;
   
