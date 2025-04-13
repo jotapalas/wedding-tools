82437ff3d1c5a9031f118656bcf90b39f1840af3
Juan Palacios <juan@jotapalas.dev>
2025-04-13 19:04:41 +0200

    update django settings

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -16,6 +16,8 @@ from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
