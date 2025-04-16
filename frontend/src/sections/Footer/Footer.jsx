502e8bd39ed695853a92a4ac404cc16e517ef606
Juan Palacios <juan@jotapalas.dev>
2025-04-16 18:38:23 +0200

    update footer

--- frontend/src/sections/Footer/Footer.jsx
+++ frontend/src/sections/Footer/Footer.jsx
@@ -9,10 +9,10 @@ function Footer() {
   return <section className="footer">
     <div className="footer-container">
       <p>
-        Mientras se acerca el día, echa un vistazo a <span className="bold">la playlist</span>&nbsp;
-        que estáis haciendo entre todos y ve ensayando los pasos prohibidos.&nbsp;
-        <span className="bold">¡Nos vemos en Sevilla!</span>
+        Mientras se acerca el día, echa un vistazo a <span className="bold">la playlist </span>
+        que estáis haciendo entre todos y ve ensayando los pasos prohibidos.
       </p>
+      <p className="bold">¡Nos vemos en Sevilla!</p>
       <button
         className="spotify-playlist-button"
         onClick={() => window.open(spotifyUrl, '_blank')}
